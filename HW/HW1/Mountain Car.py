27ffd774541092d68b174d06962fc62bf2a06ee3
amntheap <alireza.abbassi0@gmail.com>
2024-09-30 21:25:04 +0330

    Final changes

--- HW/HW1/Mountain Car.py
+++ HW/HW1/Mountain Car.py
@@ -1,28 +1,28 @@
 import gymnasium as gym
 import matplotlib.pyplot as plt
 
-# ایجاد محیط Car Mountain
+
 env = gym.make("MountainCar-v0", render_mode="rgb_array")
 
-# راه‌اندازی مجدد محیط
+
 observation, info = env.reset()
 
-# ذخیره داده‌ها برای موقعیت‌های خودرو
+
 positions = []
 
-# اجرای 10000 عمل تصادفی
+
 for _ in range(10000):
-    action = env.action_space.sample()  # انتخاب عمل تصادفی
-    observation, reward, done, truncated, info = env.step(action)  # انجام عمل
-    position = observation[0]  # موقعیت خودرو
-    positions.append(position)  # ذخیره موقعیت
+    action = env.action_space.sample() 
+    observation, reward, done, truncated, info = env.step(action)  
+    position = observation[0] 
+    positions.append(position) 
     
     if done or truncated:
-        observation, info = env.reset()  # راه‌اندازی مجدد محیط در صورت اتمام اپیزود
+        observation, info = env.reset()  
 
 env.close()
 
-# نمایش نمودار تغییرات موقعیت خودرو
+
 plt.plot(positions)
 plt.title('Car Position Over 10000 Random Actions')
 plt.xlabel('Step')
